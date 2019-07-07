c932ad8d6e7ac79082e0f0ccf0c71f4d728bf75e
Risa <supikateriburu@gmail.com>
2019-07-08 00:13:23 +0900

    :shower: Fix style

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -eu
 
-
 git_name=$(git log -1 --pretty=format:"%cn")
 git_email=$(git log -1 --pretty=format:"%ce")
 
 git config user.name ${git_name}
 git config user.email ${git_email}
-
 
 if git diff utapri.json >/dev/null ; then
   git add utapri.json
@@ -18,4 +16,3 @@ if git diff utapri.yml  >/dev/null ; then
   git add utapri.yml
   git commit -m ":sparkles: Update yml with utapri.json"
 fi
-
