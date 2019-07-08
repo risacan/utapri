6b6593e0d9f6a623cd2e98010ded620d4c1b2216
Risa <supikateriburu@gmail.com>
2019-07-08 23:26:34 +0900

    :wrench: Fix commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
+
 set -eu
 
 git_name=$(git log -1 --pretty=format:"%cn")
 git_email=$(git log -1 --pretty=format:"%ce")
 
-git config --local user.name ${git_name}
-git config --local user.email ${git_email}
+git config user.name ${git_name}
+git config user.email ${git_email}
 
-git diff  --exit-code --quiet -- utapri.json
+set +e
+
+git diff --exit-code --quiet -- utapri.json
 if [ 0 -ne $? ]; then
-    git add utapri.json
+  git add utapri.json
   git commit -m ":shower: Sort utapri.json"
   git push
 fi
 
-git diff  --exit-code --quiet -- utapri.yml
+git diff --exit-code --quiet -- utapri.yml
 if [ 0 -ne $? ]; then
   git add utapri.yml
   git commit -m ":sparkles: Update yml with utapri.json"
