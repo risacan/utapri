60900684946978428a47acfd099a6befc75c9ce5
Risa <supikateriburu@gmail.com>
2019-07-08 23:43:03 +0900

    :wrench: Change repository name
    fixup! :wrench: Change repository name

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,6 +5,7 @@ set -eu
 git_name=$(git log -1 --pretty=format:"%cn")
 git_email=$(git log -1 --pretty=format:"%ce")
 branch_name=$(git symbolic-ref --short HEAD)
+repository_name="https://${PERSONAL_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
 git config user.name ${git_name}
 git config user.email ${git_email}
@@ -15,12 +16,12 @@ git diff --exit-code --quiet -- utapri.json
 if [ 0 -ne $? ]; then
   git add utapri.json
   git commit -m ":shower: Sort utapri.json"
-  git push origin ${branch_name}
+  git push ${repository_name} ${branch_name}
 fi
 
 git diff --exit-code --quiet -- utapri.yml
 if [ 0 -ne $? ]; then
   git add utapri.yml
   git commit -m ":sparkles: Update yml with utapri.json"
-  git push origin ${branch_name}
+  git push ${repository_name} ${branch_name}
 fi
