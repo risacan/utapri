161419c7c5b39f9f21e27a15216da98a2999f104
Risa Harada <supikateriburu@gmail.com>
2020-07-23 22:39:15 +0900

    :apple:

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -11,7 +11,7 @@ git diff --exit-code --quiet -- utapri.json
 if [ 0 -ne $? ]; then
   git add utapri.json
   git commit -m ":shower: Sort utapri.json"
-  git push "https://${{ secrets.PERSONAL_GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}" ${GITHUB_REF##*/}
+  git push "https://${{ secrets.PERSONAL_GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}" $GITHUB_REF
 fi
 
 git diff --exit-code --quiet -- utapri.yml
@@ -19,5 +19,5 @@ if [ 0 -ne $? ]; then
   git add utapri.yml
   git commit -m ":sparkles: Update yml with utapri.yml"
   echo $GITHUB_REF
-  git push "https://${{ secrets.PERSONAL_GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}" ${GITHUB_REF##*/}
+  git push "https://${{ secrets.PERSONAL_GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}" $GITHUB_REF
 fi
