c3228610b9215f17d152777fbae26821088bd26b
Risa Harada <supikateriburu@gmail.com>
2020-07-23 20:44:48 +0900

    :thinking:

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -4,7 +4,6 @@ set -eu
 
 git_name=$(git log -1 --pretty=format:"%cn")
 git_email=$(git log -1 --pretty=format:"%ce")
-branch_name=$(git symbolic-ref --short HEAD)
 repository_name="https://${PERSONAL_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
 git config user.name ${git_name}
@@ -16,12 +15,12 @@ git diff --exit-code --quiet -- utapri.json
 if [ 0 -ne $? ]; then
   git add utapri.json
   git commit -m ":shower: Sort utapri.json"
-  git push ${repository_name} ${branch_name}
+  git push ${repository_name}  ${GITHUB_REF##*/}
 fi
 
 git diff --exit-code --quiet -- utapri.yml
 if [ 0 -ne $? ]; then
   git add utapri.yml
   git commit -m ":sparkles: Update yml with utapri.json"
-  git push ${repository_name} ${branch_name}
+  git push ${repository_name}  ${GITHUB_REF##*/}
 fi
