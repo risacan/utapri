3a1400be01edf9a025a8301a11a4a6b8a4da9731
Risa Harada <supikateriburu@gmail.com>
2020-07-23 22:26:35 +0900

    :sparkles:

---
+++ scripts/commit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu
+
+git config user.name "$(git log -1 --pretty=format:"%cn")"
+git config user.email "git log -1 --pretty=format:"%ce""
+
+set +e
+
+git diff --exit-code --quiet -- utapri.json
+if [ 0 -ne $? ]; then
+  git add utapri.json
+  git commit -m ":shower: Sort utapri.json"
+  git push "https://${{ secrets.PERSONAL_GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}" ${GITHUB_REF##*/}
+fi
+
+git diff --exit-code --quiet -- utapri.yml
+if [ 0 -ne $? ]; then
+  git add utapri.yml
+  git commit -m ":sparkles: Update yml with utapri.json"
+  git push "https://${{ secrets.PERSONAL_GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}" ${GITHUB_REF##*/}
